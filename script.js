ec718f6aafce9a4d11ee1c8f5c6589b20c4a9058
Sebastian Doe <sebastiandoe5@gmail.com>
2019-10-17 17:38:35 +0100

    Prevent F12 and Ctrl Shift I

--- script.js
+++ script.js
@@ -45,3 +45,11 @@ setTimeout(function() {
 });
 
 document.addEventListener('contextmenu', event => event.preventDefault());
+
+$(document).keydown(function (event) {
+    if (event.keyCode == 123) { // Prevent F12
+        return false;
+    } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I        
+        return false;
+    }
+});
